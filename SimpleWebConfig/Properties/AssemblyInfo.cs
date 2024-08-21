4af1d2d8781fe334331c6db582b16f8018f36ec9
Sergey <s@cemin.ru>
2024-08-21 11:41:58 +0500

    Обновил nuget Lers FW до 3.58.1

--- SimpleWebConfig/Properties/AssemblyInfo.cs
+++ SimpleWebConfig/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.58.0")]
+[assembly: AssemblyFileVersion("3.58.0")]
 [assembly: AssemblyPlugin("0a1b6f36-629f-4dc6-8101-ec6ae9a1722d")]
 [assembly: AssemblyPluginWebsite("https://github.com/s7emin")]
 [assembly: AssemblyPluginEmail("s@cemin.ru")]
